1c841c2cf496452b2b7c7a05025b25848d39c999
chethan-gen <chaithuregala123@gmailcom>
2025-04-11 13:37:11 +0530

    adding files

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,1 +1,13 @@
 // write button card here
+import React from 'react';
+import './button.css'; // Optional: Add styles in this file
+
+const ViewButton = () => {
+  return (
+    <button className="viewButton">
+      View Product
+    </button>
+  );
+};
+
+export default ViewButton;
